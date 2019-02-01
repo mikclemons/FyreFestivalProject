001c2f02e29aadac631bfb9f3f5aac01e7d8b49a
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-01 13:27:53 -0500

    Starting RegistrationForm class. Create constructor for RegistrationForm class. Create accessor methods for RegistrationForm class.

---
+++ src/com/company/RegistrationForm.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class RegistrationForm {
+
+    private String name;
+    private String address;
+    private String email;
+    private String arrivalDate;
+    private String specialRequest;
+    private int idNumber;
+
+    public RegistrationForm(String name, String address, String email, String arrivalDate, String specialRequest){
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.arrivalDate = arrivalDate;
+        this.specialRequest = specialRequest;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(String arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public String getSpecialRequest() {
+        return specialRequest;
+    }
+
+    public void setSpecialRequest(String specialRequest) {
+        this.specialRequest = specialRequest;
+    }
+}
